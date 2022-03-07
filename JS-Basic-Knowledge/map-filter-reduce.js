72f123bc863f18a27c859ddbb57fefd7b3d50887
Nathanael-Jason <71364489+Nathanael-Jason@users.noreply.github.com>
2022-03-07 19:32:10 +0700

    Create map-filter-reduce.js

---
+++ JS-Basic-Knowledge/map-filter-reduce.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(item => item * 2);
+console.log(doubled);
+
+var new_array = arr.filter(function callback(element, index, array) {
+} [, thisArg])
+
+const numbers = [1, 2, 3, 4];
+const sum = numbers.reduce(function (result, item) {
+  return result + item;
+}, 0);
+console.log(sum); // 10
